c99a57c7ae7c1b9c4ec2daceda53b14e368bbd63
B-JCoder <bilalcodes777@gmail.com>
2026-01-09 10:26:10 +0500

    error solve

--- views/guards/super-admin.blade.php
+++
@@ -1,8 +0,0 @@
-<script>
-    const token = localStorage.getItem('token');
-    const role  = localStorage.getItem('loginRole');
-
-    if (!token || role !== 'superAdmin') {
-        window.location.href = "{{ route('saas.login.view') }}";
-    }
-</script>
