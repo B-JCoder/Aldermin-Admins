6e6914a7975292f738153d9473b8aff141f3e750
B-JCoder <bilalcodes777@gmail.com>
2026-01-27 23:53:40 +0500

    add folders

---
+++ app/admin/student-info/studentexport/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const StudentExportPage = () => {
+  return (
+    <div>
+      <h1>Student Export</h1>
+      <button>Export</button>
+    </div>
+  );
+};
+
+export default StudentExportPage;
